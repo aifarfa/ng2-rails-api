4391f8400303a86f509a81704a987172e1b15e83
pasit.r <kzneko@gmail.com>
2017-03-08 22:44:27 +0700

    implement :show endpoint

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -4,5 +4,13 @@ class Api::ProductsController < ApplicationController
     products = Product.all
     render json: products, status: :ok
   end
-  
+
+  def show
+    begin
+      product = Product.find(params[:id])
+      render json: product, status: :ok
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { message: e.message }, status: :not_found 
+    end
+  end
 end
--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -19,4 +19,32 @@ RSpec.describe 'Products API', type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe 'GET /api/products/:id' do
+    before { get "/api/products/#{product_id}" }
+
+    context 'when the record exists' do
+      it 'returns the product' do
+        json = JSON.parse(response.body)
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(product_id)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the record does not exist' do
+      let(:product_id) { 100 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Product/)
+      end
+    end
+  end
 end
