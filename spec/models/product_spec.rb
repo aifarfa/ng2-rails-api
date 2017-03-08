3e6d09f1a92f310555eae62cd11bbd50e5e750c5
pasit.r <kzneko@gmail.com>
2017-03-08 22:10:36 +0700

    more product spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:sku) }
+
+  describe 'default attributes' do
+    before { @product = FactoryGirl.build(:product) }
+    subject { @product }
+
+    it { should be_valid }
+
+    it 'has default avalable value' do
+      expect(@product.available).to_not be_nil
+    end
+  end
+
 end
