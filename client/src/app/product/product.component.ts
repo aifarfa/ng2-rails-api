961cade2b6211e21b346cc2a7fbcca3d1811c042
pasit.r <kzneko@gmail.com>
2017-03-09 17:44:24 +0700

    dummy products

--- client/src/app/product/product.component.ts
+++ client/src/app/product/product.component.ts
@@ -9,7 +9,13 @@ export class ProductComponent implements OnInit {
 
   constructor() { }
 
+  products: any[] = [
+    { name: 'Foo', available: 200 },
+    { name: 'Bar', available: 100 }
+  ]
+
   ngOnInit() {
+    // console.log(this.products)
   }
 
 }
