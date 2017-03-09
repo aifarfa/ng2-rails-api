90e3c424764c094d91bd4631a46d4aef96646536
pasit.r <kzneko@gmail.com>
2017-03-09 14:01:40 +0700

    implement :create endpoint

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,16 +1,26 @@
 class Api::ProductsController < ApplicationController
-
   def index
     products = Product.all
     render json: products, status: :ok
   end
 
   def show
-    begin
-      product = Product.find(params[:id])
-      render json: product, status: :ok
-    rescue ActiveRecord::RecordNotFound => e
-      render json: { message: e.message }, status: :not_found 
-    end
+    product = Product.find(params[:id])
+    render json: product, status: :ok
+  rescue ActiveRecord::RecordNotFound => e
+    render json: { errors: e.message }, status: :not_found
+  end
+
+  def create
+    product = Product.create!(product_params)
+    render json: product, status: :created
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { errors: e.message }, status: :unprocessable_entity
+  end
+
+  protected
+
+  def product_params
+    params.permit(:name, :sku, :available)
   end
 end
--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -47,4 +47,44 @@ RSpec.describe 'Products API', type: :request do
       end
     end
   end
+
+  describe 'POST /api/products' do
+    # valid payload
+    let(:valid_attributes) { { name: 'Smart', sku: 'test-001', available: 0 } }
+
+    context 'when the request is valid' do
+      before { post '/api/products', params: valid_attributes }
+
+      it 'creates a product' do
+        json = JSON.parse(response.body)
+        expect(json['name']).to eq('Smart')
+      end
+
+      it 'save new record' do
+        expect(Product.count).to eq(11)
+      end
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'when the request is invalid' do
+      before { post '/api/products', params: { name: 'Foobar' } }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'does not save invalid product' do
+        expect(Product.count).to eq(10)
+      end
+
+      it 'returns a validation failure message' do
+        expect(response.body)
+          .to match(/Validation failed/)
+      end
+    end
+  end
+
 end
