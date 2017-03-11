0cb087e1a5e9782c1f6da33a83fdd7b9d32e0a90
pasit.r <kzneko@gmail.com>
2017-03-11 19:28:59 +0700

    fixed routes config for root

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root controller: 'static', action: '/'
-  root controller: 'static', action: '/public/index.html'
-  
+  root controller: 'static', action: '/'
+
   namespace :api do
     resources :products, only: [:index, :show, :create, :update]
   end
+
+  get '*other', to: 'static#index'
 end
