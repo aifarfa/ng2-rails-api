0698f2ebb3943a4cbd4e5a45d2adf30eaf49f3f4
pasit.r <kzneko@gmail.com>
2017-03-11 19:09:21 +0700

    add static routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root controller: 'static', action: '/'
+  root controller: 'static', action: '/public/index.html'
+  
   namespace :api do
     resources :products, only: [:index, :show, :create, :update]
   end
