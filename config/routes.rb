950019e90c76b89e07adc7def32daea9422db478
pasit.r <kzneko@gmail.com>
2017-03-11 20:00:57 +0700

    reverted static files controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root controller: 'static', action: '/'
-
   namespace :api do
     resources :products, only: [:index, :show, :create, :update]
   end
-
-  get '*other', to: 'static#index'
 end
